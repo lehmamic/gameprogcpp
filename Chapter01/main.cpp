2360708861775f21ce1cee7d9d81db1be1062f51
Michael Lehmann <michael.lehmann1@swisscom.com>
2021-01-09 23:26:06 +0100

    Implement Chapter 01

---
+++ Chapter01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Game.h"
+
+int main() {
+    Game game;
+    bool success = game.Initialize();
+
+    if (success) {
+        game.RunLoop();
+    }
+
+    game.Shutdown();
+
+    return 0;
+}
