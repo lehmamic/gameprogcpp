564567d1a6a3a2ee02a373e0d14006d2d7bf11bf
Michael Lehmann <michael.lehmann1@swisscom.com>
2021-01-26 14:03:21 +0100

    Chapter05_Veldrid: Set world transform matrix again

--- Chapter05_Veldrid/Game.cs
+++ Chapter05_Veldrid/Game.cs
@@ -114,19 +114,19 @@ namespace Chapter05
         {
             // Compute delta time (as in Chapter 1)
             // Wait until 16ms has elapsed since last frame
-            while (_stopwatch.ElapsedTicks < _ticksCount + 16 * TimeSpan.TicksPerSecond)
-            {
-            }
+            // while (_stopwatch.ElapsedTicks < _ticksCount + 16 * TimeSpan.TicksPerSecond)
+            // {
+            // }
 
             // Delta time is the difference in ticks from last frame
             // (converted to seconds)
             float deltaTime = (_stopwatch.ElapsedTicks - _ticksCount) / (1.0f * TimeSpan.TicksPerSecond);
 
             // Clamp maximum delta time value
-            if (deltaTime > 0.05f)
-            {
-                deltaTime = 0.05f;
-            }
+            // if (deltaTime > 0.05f)
+            // {
+            //     deltaTime = 0.05f;
+            // }
 
             // Update tick counts (for next frame)
             _ticksCount = _stopwatch.ElapsedTicks;
@@ -161,10 +161,10 @@ namespace Chapter05
         private void LoadData()
         {
             // Create player's ship
-            _ship = new Ship(this)
-            {
-                Rotation = MathUtils.PiOver2,
-            };
+            // _ship = new Ship(this)
+            // {
+            //     Rotation = MathUtils.PiOver2,
+            // };
 
             // Create asteroids
             const int numAsteroids = 20;
